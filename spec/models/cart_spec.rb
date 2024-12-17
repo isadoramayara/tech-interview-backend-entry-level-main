1ccd1b93f3f7aac6886d9fe9083ab9dc07aa7bc0
Isadóra Silva <isadoradas.esilva@gmail.com>
2024-12-17 19:50:02 -0300

    feat: more tests to carts

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -7,6 +7,26 @@ RSpec.describe Cart, type: :model do
       expect(cart.valid?).to be_falsey
       expect(cart.errors[:total_price]).to include("must be greater than or equal to 0")
     end
+
+    it 'validates numericality of total_price with valid values' do
+      cart = described_class.new(total_price: 0)
+      expect(cart.valid?).to be_truthy
+
+      cart.total_price = 100
+      expect(cart.valid?).to be_truthy
+    end
+  end
+
+  describe 'set_total_price' do
+    it 'calculates total_price based on cart_products' do
+      product1 = create(:product, price: 10)
+      product2 = create(:product, price: 20)
+      cart = create(:shopping_cart)
+      cart.cart_products.create(product: product1, quantity: 2)
+      cart.cart_products.create(product: product2, quantity: 3)
+
+      expect(cart.total_price).to eq(10 * 2 + 20 * 3)
+    end
   end
 
   describe 'mark_as_abandoned' do
@@ -16,6 +36,11 @@ RSpec.describe Cart, type: :model do
       shopping_cart.update(last_interaction_at: 3.hours.ago)
       expect { shopping_cart.mark_as_abandoned }.to change { shopping_cart.abandoned? }.from(false).to(true)
     end
+
+    it 'does not mark cart as abandoned if hour limit is not exceeded' do
+      shopping_cart.update(last_interaction_at: 1.hour.ago)
+      expect { shopping_cart.mark_as_abandoned }.not_to change { shopping_cart.abandoned_at }
+    end
   end
 
   describe 'remove_if_abandoned' do
@@ -24,6 +49,12 @@ RSpec.describe Cart, type: :model do
     it 'removes the shopping cart if abandoned for a certain time' do
       shopping_cart.mark_as_abandoned
       expect { shopping_cart.remove_if_abandoned }.to change { Cart.count }.by(-1)
+    end
+
+    it 'does not remove the shopping cart if not abandoned long enough' do
+      shopping_cart.update(last_interaction_at: 3.days.ago)
+      shopping_cart.mark_as_abandoned
+      expect { shopping_cart.remove_if_abandoned }.not_to change { Cart.count }
     end
   end
 end
