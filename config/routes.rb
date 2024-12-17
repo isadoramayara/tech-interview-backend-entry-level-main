690289238311b14e097b7cdcc0e0c75ce789991d
Isadóra Silva <isadoradas.esilva@gmail.com>
2024-12-17 10:57:21 -0300

    feat: routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   resources :products
 
-  resources :carts
+  get '/cart', to: 'carts#show'
+  patch '/cart/add_item', to: 'carts#update'
+  delete '/cart/:product_id', to: 'carts#destroy'
 
   get "up" => "rails/health#show", as: :rails_health_check
 
