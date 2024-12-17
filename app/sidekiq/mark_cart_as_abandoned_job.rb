8c6d2d30edfd5904b80f317714f06c2ddd41be89
Isadóra Silva <isadoradas.esilva@gmail.com>
2024-12-17 10:29:39 -0300

    feat: mark as abandoned job

--- app/sidekiq/mark_cart_as_abandoned_job.rb
+++ app/sidekiq/mark_cart_as_abandoned_job.rb
@@ -1,7 +1,24 @@
 class MarkCartAsAbandonedJob
   include Sidekiq::Job
+  queue_as :default
 
   def perform(*args)
-    # TODO Impletemente um Job para gerenciar, marcar como abandonado. E remover carrinhos sem interação. 
+    mark_carts_as_abandoned
+
+    delete_abandoned_carts
+  end
+
+  private
+
+  def mark_carts_as_abandoned
+    Cart.inactive_for(3.hours.ago).find_each do |cart|
+      cart.update!(status: :abandoned)
+    end
+  end
+
+  def delete_abandoned_carts
+    Cart.abandoned_for(7.days.ago).find_each do |cart|
+      cart.destroy!
+    end
   end
 end
