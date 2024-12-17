a48daa7901e128238a91c4682a54fa2dfded0936
Isadóra Silva <isadoradas.esilva@gmail.com>
2024-12-17 11:17:55 -0300

    feat: validate cart and product presence

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -1,4 +1,7 @@
 class CartProduct < ApplicationRecord
   belongs_to :cart, autosave: true, touch: true
   belongs_to :product, autosave: true
+
+  validates :cart, :product, presence: true
+  validates_numericality_of :quantity, greater_than_or_equal_to: 0
 end
