f11b09789597a582bf2b75b6da79381c1d2c770f
Isadóra Silva <isadoradas.esilva@gmail.com>
2024-12-17 10:30:04 -0300

    feat: make quantity acessable for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,13 @@
 class Product < ApplicationRecord
+  has_many :cart_products, dependent: :destroy
+  has_many :carts, through: :cart_products
+
   validates_presence_of :name, :price
   validates_numericality_of :price, greater_than_or_equal_to: 0
+
+  def quantity(cart)
+    cart_product = cart_products.find_by(cart:)
+
+    cart_product.quantity
+  end
 end
